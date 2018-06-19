9153a16c1abe2cc466fc8cdfbf881b4551e50603
cassidytrinh <cassidytrinh@gmail.com>
2018-06-19 15:45:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,22 +1,22 @@
 
-# def fizzbuzz(num)
-#   ret = ""
-#   if num%3 == 0
-#     ret += "Fizz"
-#   end
-#   if num%5 == 0
-#     ret += "Buzz"
-#   end
-#   puts ret
-# end
 def fizzbuzz(num)
-  if num%3 == 0 && num%5 == 0
-    return "FizzBuzz"
-  elsif num%3 == 0
-    return "Fizz"
-  elsif num%5 == 0
-    return "Buzz"
-  else
-    return nil
+  ret = ""
+  if num%3 == 0
+    ret += "Fizz"
   end
+  if num%5 == 0
+    ret += "Buzz"
+  end
+  return ret
 end
+# def fizzbuzz(num)
+#   if num%3 == 0 && num%5 == 0
+#     return "FizzBuzz"
+#   elsif num%3 == 0
+#     return "Fizz"
+#   elsif num%5 == 0
+#     return "Buzz"
+#   else
+#     return nil
+#   end
+# end
